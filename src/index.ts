c706134a4befaf6eb6972cced6c8e2e29d7bb6a5
Petr Tsymbarovich <petr@tsymbarovich.ru>
2019-07-08 12:48:13 +0300

    Get element by ID not query

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import './index.css'
 import('./main')
   .then((main) => (main.default || main)())
   .catch(() => {
-    const div = document.querySelector<HTMLDivElement>('#nc-loading')
+    const div = document.getElementById('nc-loading')
     if (div) {
       div.innerText = 'Internal error'
     }
